fcbc8bf5bb135fd2b4c369c403d03797f749ce98
huyangyang <hyy1110374@126.com>
2018-01-02 14:38:41 +0800

    修改人：胡泱泱 修改内容：写手首页栏目模块

---
+++ writer-openapi/src/main/java/com/yryz/writer/indexColumn/IndexColumnController.java
@@ -0,0 +1,35 @@
+package com.yryz.writer.indexColumn;
+
+import com.yryz.component.rpc.RpcResponse;
+import com.yryz.component.rpc.dto.PageList;
+
+import com.yryz.writer.modules.indexcolumn.vo.IndexColumnVo;
+import com.yryz.writer.modules.indexcolumn.dto.IndexColumnDto;
+import com.yryz.writer.modules.indexcolumn.entity.IndexColumn;
+import com.yryz.writer.modules.indexcolumn.IndexColumnApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("services/app/v1/indexColumn")
+public class IndexColumnController {
+   @Autowired
+   private IndexColumnApi indexColumnApi;
+
+   @ResponseBody
+   @RequestMapping(value="/single", method = RequestMethod.GET)
+   public RpcResponse<IndexColumnVo> detail(Long indexColumnId) {
+       return indexColumnApi.detail(indexColumnId);
+   }
+
+   @ResponseBody
+   @RequestMapping(value="/list", method = RequestMethod.GET)
+   public RpcResponse<PageList<IndexColumnVo>> list(IndexColumnDto indexColumnDto) {
+        return indexColumnApi.list(indexColumnDto);
+   }
+
+}
