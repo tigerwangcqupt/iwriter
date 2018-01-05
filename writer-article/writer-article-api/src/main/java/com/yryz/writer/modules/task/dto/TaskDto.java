2df59cf14e9b56af1fa0bf6243ff876d9a87cb7a
luohao <luohao@yryz.com>
2018-01-05 16:04:40 +0800

    YR-1000 [接口]写手个人主页-平台任务（写手任务列表）

--- writer-article/writer-article-api/src/main/java/com/yryz/writer/modules/task/dto/TaskDto.java
+++ writer-article/writer-article-api/src/main/java/com/yryz/writer/modules/task/dto/TaskDto.java
@@ -3,12 +3,19 @@ package com.yryz.writer.modules.task.dto;
 import com.yryz.component.rpc.dto.PageList;
 
 /**
+ * @author luohao
  * @ClassName: TaskDto
  * @Description: TaskDto
- * @author luohao
  * @date 2018-01-02 20:07:17
- *
  */
 public class TaskDto extends PageList {
+    private Integer status;
 
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
