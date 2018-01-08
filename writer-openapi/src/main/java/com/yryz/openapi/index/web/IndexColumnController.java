b3245645ffd8ce3f6f15ce51b5facc763c55e823
huyangyang <hyy1110374@126.com>
2018-01-08 17:54:12 +0800

    修改人：胡泱泱 任务号:xx 修改： 增加首页栏目查询日志

--- writer-openapi/src/main/java/com/yryz/openapi/index/web/IndexColumnController.java
+++ writer-openapi/src/main/java/com/yryz/openapi/index/web/IndexColumnController.java
@@ -5,6 +5,8 @@ import com.yryz.writer.modules.indexcolumn.IndexColumnApi;
 import com.yryz.writer.modules.indexcolumn.dto.IndexColumnDto;
 import com.yryz.writer.modules.indexcolumn.vo.IndexColumnVo;
 import com.yryz.writer.modules.message.vo.IndexTipsVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
@@ -15,6 +17,8 @@ import java.util.List;
 @Controller
 @RequestMapping("services/app/v1/indexColumn")
 public class IndexColumnController {
+
+   private static final Logger logger = LoggerFactory.getLogger(IndexColumnController.class);
 
    @Autowired
    private IndexColumnApi indexColumnApi;
@@ -29,6 +33,7 @@ public class IndexColumnController {
    @RequestMapping(value="/list", method = RequestMethod.GET)
    public RpcResponse<IndexColumnVo> list(IndexColumnDto indexColumnDto, @RequestHeader Long userId) {
       Assert.notNull(userId, "写手id不能为空");
+      logger.info("------进入首页栏目controller-----");
       indexColumnDto.setCustId(userId);
 //        return indexColumnApi.list(indexColumnDto);
       return indexColumnApi.listByWriter(indexColumnDto);
