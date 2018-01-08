2aafa524f4c8fa22e63cb02b72fd8cf1e86c698e
huyangyang <hyy1110374@126.com>
2018-01-08 19:26:34 +0800

    修改人：胡泱泱 任务号:YR-996 修改： 修改文章分享列表

--- writer-event/writer-event-api/src/main/java/com/yryz/writer/modules/articleshare/vo/ArticleShareVo.java
+++ writer-event/writer-event-api/src/main/java/com/yryz/writer/modules/articleshare/vo/ArticleShareVo.java
@@ -1,5 +1,7 @@
 package com.yryz.writer.modules.articleshare.vo;
 import java.io.Serializable;
+import java.util.Date;
+
 import com.yryz.writer.modules.articleshare.entity.ArticleShare;
 
 /**
@@ -11,5 +13,68 @@ import com.yryz.writer.modules.articleshare.entity.ArticleShare;
  */
 public class ArticleShareVo implements Serializable {
 
+    /**
+     * 文章标题
+     */
+    private  String articleTitle;
 
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+
+    /**
+     * 创建者id(分享用户id)
+     */
+    private  Long userId;
+
+    /**
+     * 内容：【用户昵称】分享了你的【文章标题】
+     */
+    private String content;
+
+    /**
+     * 创建者昵称
+     */
+    private  String createUserNickname;
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getCreateUserNickname() {
+        return createUserNickname;
+    }
+
+    public void setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreateUserNickname(String createUserNickname) {
+        this.createUserNickname = createUserNickname;
+    }
 }
