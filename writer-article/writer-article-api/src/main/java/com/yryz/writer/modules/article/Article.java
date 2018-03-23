32b5b3b112dd29d8b861792c87b06c01e40f2442
liuchangli <liuchangli@yryz.com>
2018-03-23 14:02:52 +0800

    文章标签实体类修改

--- writer-article/writer-article-api/src/main/java/com/yryz/writer/modules/article/Article.java
+++ writer-article/writer-article-api/src/main/java/com/yryz/writer/modules/article/Article.java
@@ -5,8 +5,10 @@ package com.yryz.writer.modules.article;
 
 
 import com.yryz.writer.common.entity.GenericEntity;
+import com.yryz.writer.modules.articlelabel.entity.ArticleLabel;
 
 import java.util.Date;
+import java.util.List;
 
 public class Article extends GenericEntity {
 	private Long tid;
@@ -53,6 +55,10 @@ public class Article extends GenericEntity {
 
 	private String articleType;
 
+	// ext
+	private String columnName;
+	private String appliName;
+
 	private int maxRecommendSort;
 
 	private int minRecommendSort;
@@ -62,6 +68,8 @@ public class Article extends GenericEntity {
 	private Long writerKid;
 
 	private String coverImgUrl;
+
+	private Integer coverImgType;
 
 	private String contentHtml;
 
@@ -85,16 +93,233 @@ public class Article extends GenericEntity {
 
 	private String labelIds;
 
+	private List<ArticleLabel> labelList;
+
 	private String contentHtml2;
 
 	private Integer waitShelve;
 
-	public String getContentHtml2() {
-		return contentHtml2;
+	// 发布文章的作者类型:0 写手,1 悠然一指用户,2 运营人员
+	private Integer createUserType;
+	// 视频时长
+	private Integer videoDuration;
+	// 文章标签管理表id
+	private Long articleLabelKid;
+
+	public Long getTid() {
+		return tid;
 	}
 
-	public void setContentHtml2(String contentHtml2) {
-		this.contentHtml2 = contentHtml2;
+	public void setTid(Long tid) {
+		this.tid = tid;
+	}
+
+	public Long getAppId() {
+		return appId;
+	}
+
+	public void setAppId(Long appId) {
+		this.appId = appId;
+	}
+
+	public Long getColumnId() {
+		return columnId;
+	}
+
+	public void setColumnId(Long columnId) {
+		this.columnId = columnId;
+	}
+
+	public String getCoverPlanUrl() {
+		return coverPlanUrl;
+	}
+
+	public void setCoverPlanUrl(String coverPlanUrl) {
+		this.coverPlanUrl = coverPlanUrl;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAudioUrl() {
+		return audioUrl;
+	}
+
+	public void setAudioUrl(String audioUrl) {
+		this.audioUrl = audioUrl;
+	}
+
+	public String getVideoUrl() {
+		return videoUrl;
+	}
+
+	public void setVideoUrl(String videoUrl) {
+		this.videoUrl = videoUrl;
+	}
+
+	public String getVideoThumbnailUrl() {
+		return videoThumbnailUrl;
+	}
+
+	public void setVideoThumbnailUrl(String videoThumbnailUrl) {
+		this.videoThumbnailUrl = videoThumbnailUrl;
+	}
+
+	public String getRecommendTitle() {
+		return recommendTitle;
+	}
+
+	public void setRecommendTitle(String recommendTitle) {
+		this.recommendTitle = recommendTitle;
+	}
+
+	public String getRecommendBref() {
+		return recommendBref;
+	}
+
+	public void setRecommendBref(String recommendBref) {
+		this.recommendBref = recommendBref;
+	}
+
+	public String getRecommendBigmap() {
+		return recommendBigmap;
+	}
+
+	public void setRecommendBigmap(String recommendBigmap) {
+		this.recommendBigmap = recommendBigmap;
+	}
+
+	public Integer getSort() {
+		return sort;
+	}
+
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+
+	public Integer getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(Integer recommend) {
+		this.recommend = recommend;
+	}
+
+	public Integer getShelveFlag() {
+		return shelveFlag;
+	}
+
+	public void setShelveFlag(Integer shelveFlag) {
+		this.shelveFlag = shelveFlag;
+	}
+
+	public Integer getDelFlag() {
+		return delFlag;
+	}
+
+	public void setDelFlag(Integer delFlag) {
+		this.delFlag = delFlag;
+	}
+
+	public Integer getViewCount() {
+		return viewCount;
+	}
+
+	public void setViewCount(Integer viewCount) {
+		this.viewCount = viewCount;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContentSource() {
+		return contentSource;
+	}
+
+	public void setContentSource(String contentSource) {
+		this.contentSource = contentSource;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Integer getSendFlag() {
+		return sendFlag;
+	}
+
+	public void setSendFlag(Integer sendFlag) {
+		this.sendFlag = sendFlag;
+	}
+
+	public String getArticleType() {
+		return articleType;
+	}
+
+	public void setArticleType(String articleType) {
+		this.articleType = articleType;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+
+	public String getAppliName() {
+		return appliName;
+	}
+
+	public void setAppliName(String appliName) {
+		this.appliName = appliName;
+	}
+
+	public int getMaxRecommendSort() {
+		return maxRecommendSort;
+	}
+
+	public void setMaxRecommendSort(int maxRecommendSort) {
+		this.maxRecommendSort = maxRecommendSort;
+	}
+
+	public int getMinRecommendSort() {
+		return minRecommendSort;
+	}
+
+	public void setMinRecommendSort(int minRecommendSort) {
+		this.minRecommendSort = minRecommendSort;
+	}
+
+	public Integer getSystemType() {
+		return systemType;
+	}
+
+	public void setSystemType(Integer systemType) {
+		this.systemType = systemType;
 	}
 
 	public Long getWriterKid() {
@@ -111,6 +336,14 @@ public class Article extends GenericEntity {
 
 	public void setCoverImgUrl(String coverImgUrl) {
 		this.coverImgUrl = coverImgUrl;
+	}
+
+	public Integer getCoverImgType() {
+		return coverImgType;
+	}
+
+	public void setCoverImgType(Integer coverImgType) {
+		this.coverImgType = coverImgType;
 	}
 
 	public String getContentHtml() {
@@ -151,206 +384,6 @@ public class Article extends GenericEntity {
 
 	public void setTaskFlag(Integer taskFlag) {
 		this.taskFlag = taskFlag;
-	}
-
-	public Integer getSystemType() {
-		return systemType;
-	}
-
-	public void setSystemType(Integer systemType) {
-		this.systemType = systemType;
-	}
-
-	public int getMaxRecommendSort() {
-		return maxRecommendSort;
-	}
-
-	public void setMaxRecommendSort(int maxRecommendSort) {
-		this.maxRecommendSort = maxRecommendSort;
-	}
-
-	public int getMinRecommendSort() {
-		return minRecommendSort;
-	}
-
-	public void setMinRecommendSort(int minRecommendSort) {
-		this.minRecommendSort = minRecommendSort;
-	}
-
-	public Long getTid() {
-		return tid;
-	}
-
-	public void setTid(Long tid) {
-		this.tid = tid;
-	}
-
-	public Long getAppId() {
-		return appId;
-	}
-
-	public void setAppId(Long appId) {
-		this.appId = appId;
-	}
-
-	public Long getColumnId() {
-		return columnId;
-	}
-
-	public void setColumnId(Long columnId) {
-		this.columnId = columnId;
-	}
-
-	public String getCoverPlanUrl() {
-		return coverPlanUrl;
-	}
-
-	public void setCoverPlanUrl(String coverPlanUrl) {
-		this.coverPlanUrl = coverPlanUrl == null ? null : coverPlanUrl.trim();
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title == null ? null : title.trim();
-	}
-
-	public Integer getRecommend() {
-		return recommend;
-	}
-
-	public void setRecommend(Integer recommend) {
-		this.recommend = recommend;
-	}
-
-	public Integer getSort() {
-		return sort;
-	}
-
-	public void setSort(Integer sort) {
-		this.sort = sort;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description == null ? null : description.trim();
-	}
-
-	public String getAudioUrl() {
-		return audioUrl;
-	}
-
-	public void setAudioUrl(String audioUrl) {
-		this.audioUrl = audioUrl == null ? null : audioUrl.trim();
-	}
-
-	public String getVideoUrl() {
-		return videoUrl;
-	}
-
-	public void setVideoUrl(String videoUrl) {
-		this.videoUrl = videoUrl == null ? null : videoUrl.trim();
-	}
-
-	public String getVideoThumbnailUrl() {
-		return videoThumbnailUrl;
-	}
-
-	public void setVideoThumbnailUrl(String videoThumbnailUrl) {
-		this.videoThumbnailUrl = videoThumbnailUrl == null ? null : videoThumbnailUrl.trim();
-	}
-
-	public String getRecommendTitle() {
-		return recommendTitle;
-	}
-
-	public void setRecommendTitle(String recommendTitle) {
-		this.recommendTitle = recommendTitle == null ? null : recommendTitle.trim();
-	}
-
-	public String getRecommendBref() {
-		return recommendBref;
-	}
-
-	public void setRecommendBref(String recommendBref) {
-		this.recommendBref = recommendBref == null ? null : recommendBref.trim();
-	}
-
-	public String getRecommendBigmap() {
-		return recommendBigmap;
-	}
-
-	public void setRecommendBigmap(String recommendBigmap) {
-		this.recommendBigmap = recommendBigmap == null ? null : recommendBigmap.trim();
-	}
-
-	public Integer getShelveFlag() {
-		return shelveFlag;
-	}
-
-	public void setShelveFlag(Integer shelveFlag) {
-		this.shelveFlag = shelveFlag;
-	}
-
-	public Integer getDelFlag() {
-		return delFlag;
-	}
-
-	public void setDelFlag(Integer delFlag) {
-		this.delFlag = delFlag;
-	}
-
-	public Integer getViewCount() {
-		return viewCount;
-	}
-
-	public void setViewCount(Integer viewCount) {
-		this.viewCount = viewCount;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content == null ? null : content.trim();
-	}
-
-	public String getContentSource() {
-		return contentSource;
-	}
-
-	public void setContentSource(String contentSource) {
-		this.contentSource = contentSource == null ? null : contentSource.trim();
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl == null ? null : imgUrl.trim();
-	}
-
-	public Integer getSendFlag() {
-		return sendFlag;
-	}
-
-	public void setSendFlag(Integer sendFlag) {
-		this.sendFlag = sendFlag;
-	}
-
-	public String getArticleType() {
-		return articleType;
-	}
-
-	public void setArticleType(String articleType) {
-		this.articleType = articleType;
 	}
 
 	public Integer getPayFlag() {
@@ -401,6 +434,22 @@ public class Article extends GenericEntity {
 		this.labelIds = labelIds;
 	}
 
+	public List<ArticleLabel> getLabelList() {
+		return labelList;
+	}
+
+	public void setLabelList(List<ArticleLabel> labelList) {
+		this.labelList = labelList;
+	}
+
+	public String getContentHtml2() {
+		return contentHtml2;
+	}
+
+	public void setContentHtml2(String contentHtml2) {
+		this.contentHtml2 = contentHtml2;
+	}
+
 	public Integer getWaitShelve() {
 		return waitShelve;
 	}
@@ -409,4 +458,27 @@ public class Article extends GenericEntity {
 		this.waitShelve = waitShelve;
 	}
 
+	public Integer getCreateUserType() {
+		return createUserType;
+	}
+
+	public void setCreateUserType(Integer createUserType) {
+		this.createUserType = createUserType;
+	}
+
+	public Integer getVideoDuration() {
+		return videoDuration;
+	}
+
+	public void setVideoDuration(Integer videoDuration) {
+		this.videoDuration = videoDuration;
+	}
+
+	public Long getArticleLabelKid() {
+		return articleLabelKid;
+	}
+
+	public void setArticleLabelKid(Long articleLabelKid) {
+		this.articleLabelKid = articleLabelKid;
+	}
 }
